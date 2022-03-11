13c1edb12301acda60f9c6249c03b74b2d8669ba
dhstambolliu <stambolliudhimiter@gmail.com>
2022-03-11 15:45:00 +0100

    Code Sharing Platform Completed Stage 5/5

---
+++ Topics/toString()/Users as strings/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class User {
+
+    private String login;
+    private String firstName;
+    private String lastName;
+
+    public User(String login, String firstName, String lastName) {
+        this.login = login;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "login="+login+",firstName="+firstName+",lastName="+ lastName + "\n";
+    }
+}
